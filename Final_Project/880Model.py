a4b835131341f33296f5b8494ed31f19f250164c
Jonathon Malcolm <j_malcolm@outlook.com>
2019-04-16 23:21:24 -0400

    Testing other models

--- Final_Project/880Model.py
+++ Final_Project/880Model.py
@@ -13,61 +13,49 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, confusion_matrix
 import keras
 from keras.models import Sequential
+from keras.layers import LSTM
 from keras.layers import Dense
+from keras.layers import Masking
+from keras.layers import Dropout
 from keras.layers import SimpleRNN
 
+ticker_symbol = "FB"
+
 df = pd.read_csv("880DataSet_WithClean.csv",encoding = "ISO-8859-1")
-#print(df["text"].head())
-print(df.columns)
 
-print(df.head())
+df1 = df[(df["symbol"] == ticker_symbol) & (df["tri_change"] != "2")]
 
-df1 = df[df["symbol"] == "AAPL"]
-print(df1.head())
-df1 = df1[df1["tri_change"] != "2"]
-print(df1.head())
 # Turn each day into a feature vector.
 Dates = sorted(list(set(list(df1["date"]))))
 
-#print(Dates)
-
-#print(len(Dates))
-
+#Find max number of tweets in one day
 M = 0
-for x in range(0,len(Dates)): #Max number of tweets in one day
+for x in range(0,len(Dates)):
     df2 = df1[df1["date"] == Dates[x]]
-    #print(len(df2))
     if len(df2) > M:
         M = len(df2)
     else:
         continue
-print(M)
+
 
 cols = ['symbol','date','friends_count', 'followers_count','favourites_count','volume', 'sent','bin_change','tri_change']
-subcols = ['friends_count', 'followers_count','favourites_count', 'sent']
+#subcols = ['friends_count', 'followers_count','favourites_count', 'sent']
 HStates = 5
 TotLen = len(Dates)
-Num_Feats = len(subcols)
+#Num_Feats = len(subcols)
 Window = M
 
 df = df1[cols]
 #df[subcols] = (df[subcols] - df[subcols].min()) / (df[subcols].max() - df[subcols].min())
 
-print(df.head())
 
 df1Seg = [] #x values
 dfySeg = [] # corresponding output values for each above segment
 
-for i in range(0,len(Dates)):
-    df2 = df[df["date"] == Dates[i]]
-    df3 = df2[df2["symbol"] == "AAPL"]
-    #print(df3.head())
-    dfySeg.append(int(df3["tri_change"].iloc[0]))
-    #df2 = df2[subcols]
-    #Rest = Window - len(df2)
-    #dfstack = pd.DataFrame(0, index=np.arange(Rest), columns=subcols)
-    #df2 = pd.concat([df2,dfstack])
-    
+for i in range(0, len(Dates)):  # For each date
+    df2 = df[(df["date"] == Dates[i]) & (df["symbol"] == ticker_symbol)]
+    dfySeg.append(int(df2["tri_change"].iloc[0]))
+
     # Avg
     Avg_friends_count = np.mean(df2['friends_count'].iloc[0:len(df2)].values)
     Avg_followers_count = np.mean(df2['followers_count'].iloc[0:len(df2)].values)
@@ -118,43 +106,86 @@ for i in range(0,len(Dates)):
     cumprod_sent = np.cumprod(df2['sent'].iloc[0:len(df2)].values)
     """
     Vol = df2['volume'].iloc[0]
-    Change = df2['tri_change'].iloc[0]
-    #print(Vol)
+    #Change = df2['tri_change'].iloc[0]
+
     FeatArr = [Avg_friends_count,Avg_followers_count,Avg_favourites_count,Avg_sent,
                min_friends_count,min_followers_count,min_favourites_count,min_sent,
                max_friends_count,max_followers_count,max_favourites_count,max_sent,
                std_friends_count,std_followers_count,std_favourites_count,std_sent,
                ptp_friends_count,ptp_followers_count,ptp_favourites_count,ptp_sent,
-               sum_friends_count,sum_followers_count,sum_favourites_count,sum_sent,Vol,Change]
-    
-    #print(FeatArr)
+               sum_friends_count,sum_followers_count,sum_favourites_count,sum_sent,Vol]
     
     FeatAr = np.array(FeatArr)
-    #print(FeatAr.shape)
+
     df1Seg.append(FeatAr)
-    #print(Vol)
-    
-train_X = np.zeros((TotLen,len(FeatAr)))
-train_Y = np.zeros((TotLen))
-#print(train_X[0])
-    
+
+train_X = np.zeros((TotLen, len(FeatAr)))  # 1 row per day, each row is feature vector
+train_Y = np.zeros(TotLen)
+
 for j in range(0,len(df1Seg)):
     train_X[j] = df1Seg[j]
     train_Y[j] = int(dfySeg[j])
- 
 
-#print(train_X[0])
-#print(train_Y[0])
-print(len(train_X))
-print(len(train_Y))
+train_X = np.reshape(train_X, (train_X.shape[0], train_X.shape[1], 1))  # Make shape correct for keras to be happy
 
-#print(train_X)
-train_X = np.reshape(train_X, (train_X.shape[0], train_X.shape[1], 1))
-#print(train_X)
+X_train, X_test, Y_train, Y_test = train_test_split(train_X, train_Y, test_size=.3, random_state=0)
 
-X_train, X_test, y_train, y_test = train_test_split(train_X, train_Y, test_size=.3, random_state=0)
+# hyperparameters
+epochs = 10
+batch_size = 5
+layer1Size = 20
+layer2Size = 20
+layer3Size = 10
+layer4Size = 10
+layer5Size = 5
 
-#X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+# dataset size
+inpLen = X_train.shape[1]
+inpDim = X_train.shape[2]
+print(X_train.shape)
+
+# Create model
+model = Sequential()
+
+# # CNN Model
+# model.add(Dense(layer1Size, input_dim=inpDim, activation='relu'))
+# model.add(Dropout(0.3))
+# model.add(Dense(layer2Size, activation='relu'))
+# model.add(Dropout(0.1))
+# model.add(Dense(layer3Size, activation='relu'))
+# model.add(Dropout(0.3))
+# model.add(Dense(layer4Size, activation='relu'))
+# model.add(Dropout(0.1))
+# model.add(Dense(layer5Size, activation='relu'))
+# model.add(Dense(1, activation='sigmoid'))
+
+# RNN Model
+model.add(SimpleRNN(32, activation='relu', batch_input_shape=batch_size, stateful=1))
+model.add(Dense(1, activation='sigmoid'))
+
+# # LSTM Model - Doesnt work
+# model.add(Masking(mask_value=0.0))
+# model.add(LSTM(64, return_sequences=False, dropout=0.1, recurrent_dropout=0.1))
+# model.add(Dense(1, activation='sigmoid'))
+
+
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# Fit the model
+
+history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size)
+print(model.summary())
+
+
+# Evaluate the model
+scores = model.evaluate(X_test, Y_test)
+print("Training Finished ")
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+"""
 
 model=Sequential()
 model.add(SimpleRNN(2, activation='relu', input_shape=(X_train.shape[1],X_train.shape[2])))
@@ -184,7 +215,7 @@ Recall = recall_score(y_test1, y_pred, average="macro")
 print(Accuracy)
 print(cm)
 
-"""
+
 train_X = train_X.reshape(-1, Num_Feats,Window, 1)
 
 batch_size = 64
